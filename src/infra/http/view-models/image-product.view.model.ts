984aeb09aeb3bc30963c87c7385911006ad5a898
matheus.araujo <matheus.araujo@trinusco.com.br>
2024-04-26 18:25:44 +0000

    feat: :sparkles: create find many products

---
+++ src/infra/http/view-models/image-product.view.model.ts
@@ -0,0 +1,14 @@
+import { ImageProductEntity } from 'src/app/entities/image-product.entity';
+
+export class ImageProductViewModel {
+  static toHttp(imageProduct: Partial<ImageProductEntity>) {
+    return {
+      imageProductId: imageProduct.image_product_id,
+      productId: imageProduct.productId,
+      image: imageProduct.image,
+      status: imageProduct.status,
+      createdAt: imageProduct.createdAt,
+      updatedAt: imageProduct.updatedAt,
+    };
+  }
+}
